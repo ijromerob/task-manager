73e2c84d4a4a0d2be2d9f80317ddcc1f4b0d7ba1
ijromerob <ijromerob@gmail.com>
2025-03-30 19:43:20 -0600

    switched to intellij to facilitate the run of my project

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+        val personal = PersonalTask("title", "description", "place")
+
+        var isGood = personal.isCompleted
+
+        println("lets see $isGood")
+
+        personal.changeStatus()
+
+        isGood = personal.isCompleted
+
+        println("lets see $isGood")
+}
